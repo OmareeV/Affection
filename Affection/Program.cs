e55aa8bc1998a4788bd8a91dd030b06209dfb647
Valentin_Omaree <Valentin_Omaree@edu.mcctc.net>
2020-10-22 10:29:45 -0400

    Add project files.

---
+++ Affection/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Profile
+    {
+        string name;
+        int age;
+        string city;
+        string country;
+        string pronouns;
+        string[] hobbies;
+
+        public Profile(
+              string name;
+        int age;
+        string city;
+        string country;
+        string pronouns = "they/them")
+    {
+      this.name = name;
+      this.age = age;
+      this.city = city;
+      this.country = country;
+      this.pronouns = pronouns;
+      this.hobbies = new string[0];
+    }
+    public string ViewProfile()
+    {
+        string bio = $"Name: {name}\nAge: {age}\nCity: {city}\nCountry: {country}\nPronouns: {pronouns}";
+
+        bio += "\nHobbies:\n";
+        foreach (string hobby in hobbies)
+        {
+            bio += $"- {hobby}/n";
+        }
+
+        return bio;
+    }
+    public void SetHobbies(string[] hobbies)
+    {
+        this.hobbies = hobbies
+    }
+}
+}
